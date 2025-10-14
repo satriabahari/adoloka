f6b924ce2d9737fa76ad9201acf5249288199047
satria bahari <satriaabaharii@gmail.com>
2025-10-14 15:19:40 +0700

    feat: add some seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            ProductSeeder::class,
+            ProductImagesSeeder::class,
+            CategorySeeder::class,
+            EventSeeder::class,
+            RoleSeeder::class
         ]);
     }
 }
