de0e83ca622a8e6d9392be684f744e9c6851e05c
satria bahari <satriaabaharii@gmail.com>
2025-12-18 13:40:59 +0700

    chore: app build

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/css/registration.css",
+                "resources/css/service.css",
+                "resources/css/modal.css",
                 "resources/js/app.js",
             ],
             refresh: true,
